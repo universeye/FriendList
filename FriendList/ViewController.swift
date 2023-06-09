bb97083d1dffc0908a795e88c3f63717dda1aca2
Terry Kuo <terrykuo501@gmail.com>
2023-06-09 16:49:04 +0800

    Initial Commit

---
+++ FriendList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FriendList
+//
+//  Created by Terry Kuo on 2023/6/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
